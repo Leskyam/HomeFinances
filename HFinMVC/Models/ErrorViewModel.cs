221511b833feed21ed258a173ccbe2cb068e1399
Lesky Alfonso Martinez <leskyam@gmail.com>
2021-09-22 10:32:31 -0400

    - Add MVC project

---
+++ HFinMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HFinMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
