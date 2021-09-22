b8c317fd9acfb83069a5ba082dae6c3541643a30
Lesky Alfonso Martinez <leskyam@gmail.com>
2021-09-22 12:05:12 -0400

    - "Check" controller will let us know if the Web API is woring as expected when deployed to Azure

---
+++ HFinAPI/Controllers/CheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HFinAPI.Controllers
+{
+    // [Route("api/[controller]")]
+    [Route("[controller]")]
+    [ApiController]
+    public class CheckController : ControllerBase
+    {
+
+        [HttpGet]
+        public JsonResult Get() => new JsonResult(new { success = true, message = "This Web API is running." });
+
+    }
+}
